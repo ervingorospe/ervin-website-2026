9bb097a9a3f353bfc7858c689d4bf9350832c3e5
Ervin Gorospe <ecgorospe@pcdsi.ph>
2026-05-02 01:47:11 +0800

    remove package-lock and updated state of mobile menu

--- components/layout/MenuNav.tsx
+++ components/layout/MenuNav.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Button from "@/components/Button";
 
 export default function MenuNav() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="relative block sm:hidden">
